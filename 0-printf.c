f6e8b2854f82e30c9cfd12c560efe8fc411247a7
mtmagic <mohamedhamyd5@gmail.com>
2023-09-19 00:05:23 +0100

    fixing Task 0

--- 0-printf.c
+++ 0-printf.c
@@ -3,6 +3,43 @@
 #include <string.h>
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+ 
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+* _string - handle %s.
+* @str: the string given.
+* Return: character printed.
+*/
+
+int _string(char *str)
+{
+	int cmptr = 0;
+
+	if (str == NULL)
+	{
+		str = "(null)";
+		write(1, str, 6);
+		return (6);
+	}
+	else
+	{
+		cmptr = strlen(str);
+		write(1, str, cmptr);
+		return (cmptr);
+	}
+}
+
+/**
 * _printf - produces output according to a format.
 * @format: the format specifier.
 * Return: the number of characters printed.
@@ -16,38 +53,41 @@ int _printf(const char *format, ...)
 
 
 	va_start(arg_list, format);
-	if (format == NULL)
-	{
-		write(1, "", 1);
-		return (0);
-	}
+	if (format == NULL || !*(format + 0))
+		return (-1);
 	while (*(format + i) != '\0')
 	{
-		if (*(format + i) == 'c' || *(format + i) == '%')
+		if (*(format + i) == '%')
 		{
-			value = va_arg(arg_list, int);
-			if (value == 0)
-				write(1, "", 1);
-			else
+			i++;
+			if (*(format + i) == 'c' || *(format + i) == '%')
 			{
-				write(1, &value, 1);
-				cmp++;
+				value = va_arg(arg_list, int);
+				if (value == 0)
+					write(1, "", 1);
+				else
+				{
+					write(1, &value, 1);
+					cmp++;
+				}
 			}
-		}
-		else if (*(format + i) == 's')
-		{
-			string = va_arg(arg_list, char *);
-			if (string == NULL)
+			else if (*(format + i) == 's')
 			{
-				string = "";
-				write(1, string, 1);
-			}
-			else
-			{
-				value = strlen(string);
-				write(1, string, value);
+				string = va_arg(arg_list, char *);
+				value = _string(string);
 				cmp += value;
 			}
+				else
+				{
+					_putchar('%');
+					_putchar(*(format + i));
+					cmp += 2;
+				}
+		}
+		else
+		{
+			_putchar(*(format + i));
+			cmp++;
 		}
 		i++;
 	}
