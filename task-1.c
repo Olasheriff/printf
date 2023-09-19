1807a4647a2d9079c4f262c453885ea5ba4f62fc
Olasheriff <olasheriff.99@gmail.com>
2023-09-18 18:02:35 -0700

    task-1

--- task-1.c
+++ task-1.c
@@ -1,27 +1,46 @@
 #include <stdarg.h>
 #include <stdio.h>
-int _printf(const chafr *format, ...)
+#include "main.h"
+
+/*print_int - prints integers
+ *@counts - counts characters
+ *@args - list of arguments
+ *@b - buffer array to handle
+ *@flags- calculates active flags
+ *@width- gets width
+ *@precision- precision specification
+ *@size- size specifier
+ *Return: numbers of characters printed
+ */
+int print_int(va_list args, char b[], int flags, int width, int precision, int size)
+
 {
-	va_list args;
-	va_start(args, format);
+	int BUFF_SIZE
+	int x = BUFF_SIZE - 2;
+	int is_neg = 0;
+	long int y = va_arg(args, long int);
+	unsigned long int num;
+	
+	y = convert_size_number(y, size);
 
-	int count = 0;
+	if (y == 0)
+		b[x--] = '\0';
+	
+	b[BUFF_SIZE - 1] = '\0';
+	num = (unsigned long int y); 
 
-	for (int i = 0; format[i] != '\0'; i++)
+	if (y < 0)
 	{
-		if (format[i] == '%' && (format[i + 1] == 'd' || format[i + i] == 'i'))
-		{
-			printf("%d", num);
-			count++;
-			i++;
-		}\else
-		{
-			putchar(format[i]);
-			count++;
-		}
+		num = (unsigned long int)((-1) * y);
+		is_neg = 1;
+	}
+	while (num > 0)
+	{
+		b[x--] = (num % 10) + '0';
+		num /= 10;
 	}
 
-	va_end(args)
+	x++;
 
-		return count;
+	return (write_number(is_neg, x, b, flags, width, precision, size));
 }
